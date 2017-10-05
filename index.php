6f3f5dd5a623363302639fec4387f95cad315438
Catherine Nshimiyimana <cathnshi@student.arteveldehs.be>
2017-10-05 10:32:22 +0200

    oefening3

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$x = 10;
+$y = 7;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+	<?php
+echo $x + $y, "\n";
+echo $x - $y, "\n";
+echo $x * $y, "\n";
+echo $x / $y, "\n";
+echo $x % $y, "\n";
+?>
+</body>
+</html>
